86da4d6ba8aecdc078252fdcb470de7cb34b18d2
oyekamal <autocratickamal1@gmail.com>
2023-11-26 22:57:38 +0500

    image tp pdf resize worked

--- imagesizesolution.py
+++ imagesizesolution.py
@@ -1,40 +1,37 @@
 from pdf2image import convert_from_path
 from PIL import Image
 from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
-input_pdf_path = "./toughcad/74490 BLOCK B CEILING TIE LEVEL TRUSS LAYOUT - CONSTRUCTION.pdf"
-def mm_to_points(mm):
-    return mm * 2.83465
 
-# Define your custom page sizes
-A0 = (mm_to_points(841), mm_to_points(1189))
-A1 = (mm_to_points(594), mm_to_points(841))
-A2 = (mm_to_points(420), mm_to_points(594))
-A3 = (mm_to_points(297), mm_to_points(420))
+def convert_pdf_to_image(pdf_path, output_folder):
+    images = convert_from_path(pdf_path, output_folder=output_folder)
+    return images
 
-# Sizes to be used
-sizes = {"A0": A0, "A1": A1, "A2": A2, "A3": A3}
+def resize_image(image, size):
+    resized_image = image.resize(size)
+    return resized_image
 
-# Convert PDF to images
-pages = convert_from_path(input_pdf_path)
+def convert_images_to_pdf(images, output_pdf):
+    pdf = canvas.Canvas(output_pdf)
+    for img in images:
+        pdf.setPageSize((img.width, img.height))
+        pdf.drawInlineImage(img, 0, 0)
+        pdf.showPage()
+    pdf.save()
 
-# Resize images and save them
-resized_pages = []
-for i, page in enumerate(pages):
-    # Resize page
-    resized_page = page.resize(sizes["A0"])  # Change to the size you want
-    # Save resized page to a file
-    resized_page.save(f"resized_page_{i}.png", "PNG")
-    # Add to list
-    resized_pages.append(f"resized_page_{i}.png")
+if __name__ == "__main__":
+    pdf_path = input_pdf_path = "./toughcad/74490 BLOCK B CEILING TIE LEVEL TRUSS LAYOUT - CONSTRUCTION.pdf"
 
-# Create a new PDF
-c = canvas.Canvas("resized.pdf", pagesize=sizes["A0"])  # Change to the size you want
-
-# Add images to PDF
-for page in resized_pages:
-    c.drawImage(page, 0, 0, width=sizes["A0"][0], height=sizes["A0"][1])  # Change to the size you want
-    c.showPage()
-
-# Save PDF
-c.save()
+    output_folder = "temp_images"
+    output_pdf = "resized_output.pdf"
+    
+    # Define the desired size (e.g., A4)
+    desired_size = (595, 842)  # A4 size
+    
+    # Convert PDF to images
+    images = convert_pdf_to_image(pdf_path, output_folder)
+    
+    # Resize each image
+    resized_images = [resize_image(img, desired_size) for img in images]
+    
+    # Convert resized images back to PDF
+    convert_images_to_pdf(resized_images, output_pdf)
